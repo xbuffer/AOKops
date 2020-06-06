622f663fcf700667fbc8fdd50ce0341c528aa5d7
Xbuffer <icesky624@hotmail.com>
2020-06-06 15:46:35 +0800

    让项目支持中文,设置东八区时间

--- AOKops/settings/base.py
+++ AOKops/settings/base.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'scanhosts',
+    'assets',
 ]
 
 MIDDLEWARE = [
@@ -98,9 +99,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
