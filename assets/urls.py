7f28a87daee7795d3ecd9b78a37d7d58ebbb33bf
Xbuffer <icesky624@hotmail.com>
2020-06-07 17:56:12 +0800

    添加前端仪表盘、资产表和后台首页

--- assets/urls.py
+++ assets/urls.py
@@ -4,5 +4,9 @@ from assets import views
 app_name = 'assets'
 
 urlpatterns = [
-    path('report/', views.report, name='report')
+    path('report/', views.report, name='report'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('index/', views.index, name='index'),
+    path('detail/<int:asset_id>/', views.detail, name='detail'),
+    path('', views.dashboard, name='dashboard'),
 ]
--- assets/views.py
+++ assets/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 import json
 from assets import models
@@ -30,3 +30,17 @@ def report(request):
         else:
             return HttpResponse("没有资产SN序列号请检查数据!")
     return HttpResponse('200 ok')
+
+
+def dashboard(request):
+    return render(request, 'assets/dashboard.html', locals())
+
+
+def index(request):
+    assets = models.Asset.objects.all()
+    return render(request, 'assets/index.html', locals())
+
+
+def detail(request, asset_id):
+    asset = get_object_or_404(models.Asset, id=asset_id)
+    return render(request, 'assets/detail.html', locals())
